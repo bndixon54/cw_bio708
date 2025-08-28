587334b7018005ccb8e728e2d1d3abe478c819f9
bndixon54 <baileydixon54@gmail.com>
2025-08-28 10:21:09 -0400

    tidyverse reshape

--- code/tidyverse02.R
+++ code/tidyverse02.R
@@ -32,7 +32,20 @@ df_eps <- iris_sub %>%
 #ungroup function will prevent confusing errors in the future if you arent being careful with the mutate function
 
 
+# reshape -----------------------------------------------------------------
+#this function just lets you reorganize the dataframe to view in a different arrangement. Might not always be useful. Wide format not always the best format for data organization. Akira suggests using long format bc you wont see what the values actually mean if using wide. 
 
 
+iris_w <- iris_sub %>% 
+  mutate(id = rep(1:3, 3)) %>% # add an ID column
+  select(id, Sepal.Length, Species) %>% 
+  pivot_wider(id_cols = "id", # unique row ID based on
+              values_from = "Sepal.Length", # values in each cell from
+              names_from = "Species") # new column names from
 
+#pivot longer to change to long format
+ iris_l <- iris_w %>%
+   pivot_longer(cols = c("setosa", "versicolor", "virginica"), 
+                names_to = "Species",
+                values_to = "Sepal.Length")
 
